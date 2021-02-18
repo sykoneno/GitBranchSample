77cb8b87dffc7b3c29d476bfff60be61c2cfae3f
Kaysu <sykoneno@gmail.com>
2021-02-18 14:59:14 +0800

    add Math Branch

--- GitBranchSample/src/GitBarnchApp/MainApp.java
+++ GitBranchSample/src/GitBarnchApp/MainApp.java
@@ -1,9 +1,18 @@
 package GitBarnchApp;
 
 public class MainApp {
-public static void main(String[] args) {
-	//這是第一個版本
-	System.out.println("1st Version");
-	
+	public static void main(String[] args) {
+		// 這是第一個版本
+		System.out.println("1st Version");
+
+		// 這是新增數學運算的版本
+		Math cMath = new Math();
+		System.out.println();
+		System.out.print(cMath.add(10, 5));
+	}
 }
-}
+	class Math {
+		int add(int a, int b) {
+			return a + b;
+		}
+	}
